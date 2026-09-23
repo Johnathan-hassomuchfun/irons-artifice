a620450902d0a2c87ee67d15f942579843dc5725
Johnathan-hassomuchfun <willemroosnz@gmail.com>
2026-09-23 12:02:07 +1200

    Update ModifierItem.java

--- src/main/java/io/redspace/irons_artifice/modifier/ModifierItem.java
+++ src/main/java/io/redspace/irons_artifice/modifier/ModifierItem.java
@@ -5,9 +5,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.component.TooltipDisplay;
-import org.jspecify.annotations.NonNull;
 
+import javax.annotation.Nonnull;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ModifierItem extends Item {
@@ -20,10 +20,10 @@ public class ModifierItem extends Item {
 
     @Override
     @SuppressWarnings("deprecation")
-    public void appendHoverText(@NonNull ItemStack itemStack, @NonNull TooltipContext context, @NonNull TooltipDisplay display, @NonNull Consumer<Component> builder, @NonNull TooltipFlag tooltipFlag) {
-        builder.accept(Component.empty());
-        builder.accept(Component.translatable("irons_artifice.tooltip.when_used_as_modifier").withStyle(ChatFormatting.GRAY));
-        modifier.getDescriptionText((component) -> builder.accept(Component.literal(" ").append(component)));
+    public void appendHoverText(@Nonnull ItemStack itemStack, @Nonnull TooltipContext context, @Nonnull List<Component> tooltipComponents, @Nonnull TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.empty());
+        tooltipComponents.add(Component.translatable("irons_artifice.tooltip.when_used_as_modifier").withStyle(ChatFormatting.GRAY));
+        modifier.getDescriptionText((component) -> tooltipComponents.add(Component.literal(" ").append(component)));
     }
 
     public GunModifier getModifier() {
